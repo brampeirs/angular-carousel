0d0660e095f73abe6b77ae1712327405c1a9a793
Bram Peirs <bram.peirs@showpad.com>
2019-10-10 09:40:08 +0200

    add preloading images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,71 @@
-import { Component, ViewChild } from '@angular/core';
-import { Slide } from './carousel/carousel.interface';
-import { AnimationType } from './carousel/carousel.animations';
-import { CarouselComponent } from './carousel/carousel.component';
+import { Component, ViewChild, OnInit } from "@angular/core";
+import { Slide } from "./carousel/carousel.interface";
+import { AnimationType } from "./carousel/carousel.animations";
+import { CarouselComponent } from "./carousel/carousel.component";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent  {
-    @ViewChild(CarouselComponent) carousel: CarouselComponent;
+export class AppComponent implements OnInit {
+  @ViewChild(CarouselComponent) carousel: CarouselComponent;
 
   animationType = AnimationType.Scale;
 
   animationTypes = [
     {
-      name: 'Scale',
+      name: "Scale",
       value: AnimationType.Scale
     },
     {
-      name: 'Fade',
+      name: "Fade",
       value: AnimationType.Fade
     },
     {
-      name: 'Flip',
+      name: "Flip",
       value: AnimationType.Flip
     },
     {
-      name: 'Jack In The Box',
+      name: "Jack In The Box",
       value: AnimationType.JackInTheBox
     }
-  ]
-  slides: Slide[] = 
-    [
-      {
-        headline: 'Miouw',
-        src: 'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2901&q=80'
-      },
-      {
-        headline: 'In The Wilderness',
-        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg'
-      },
-      {
-        headline: 'For Your Current Mood',
-        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg'
-      },
-      {
-        headline: 'Focus On The Writing',
-        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg'
-      }
-    ]
+  ];
+  slides: Slide[] = [
+    {
+      headline: "Miouw",
+      src:
+        "https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2901&q=80"
+    },
+    {
+      headline: "In The Wilderness",
+      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg"
+    },
+    {
+      headline: "For Your Current Mood",
+      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg"
+    },
+    {
+      headline: "Focus On The Writing",
+      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg"
+    }
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.preloadImages(); // for the demo
+  }
+
+  preloadImages() {
+    for (const slide of this.slides) {
+      new Image().src = slide.src;
+    }
+  }
 
   setAnimationType(type) {
     this.animationType = type.value;
     setTimeout(() => {
-        this.carousel.onNextClick();
-    })         
+      this.carousel.onNextClick();
+    });
   }
 }
-
