38bfe44094d39231c1d5aa8e01dbe8ae9e53ca4d
Bram Peirs <bram.peirs@showpad.com>
2019-10-17 20:15:59 +0200

    Make it even better 🦄

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,11 @@ export class AppComponent implements OnInit {
   ];
   slides: Slide[] = [
     {
+      headline: "For Your Current Mood",
+      src:
+        "https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+    },
+    {
       headline: "Miouw",
       src:
         "https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
@@ -40,11 +45,6 @@ export class AppComponent implements OnInit {
       headline: "In The Wilderness",
       src:
         "https://images.unsplash.com/photo-1557800634-7bf3c7305596?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2001&q=80"
-    },
-    {
-      headline: "For Your Current Mood",
-      src:
-        "https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
     },
     {
       headline: "Focus On The Writing",
