fa8d7e8ce48800d4c775307c184e3083647655ed
Bram Peirs <bram.peirs@showpad.com>
2019-10-17 18:28:42 +0200

    make it look more pro 💎

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,19 +34,22 @@ export class AppComponent implements OnInit {
     {
       headline: "Miouw",
       src:
-        "https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2901&q=80"
+        "https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
     },
     {
       headline: "In The Wilderness",
-      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg"
+      src:
+        "https://images.unsplash.com/photo-1557800634-7bf3c7305596?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2001&q=80"
     },
     {
       headline: "For Your Current Mood",
-      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg"
+      src:
+        "https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
     },
     {
       headline: "Focus On The Writing",
-      src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg"
+      src:
+        "https://images.unsplash.com/photo-1551410224-699683e15636?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
     }
   ];
 
